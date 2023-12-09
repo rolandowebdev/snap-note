6115a5e0bbe18775d0ad3f3f10a8138c9339f8b1
rolandowebdev <rolandopranata@gmail.com>
2023-12-09 13:23:44 +0700

    chore: change fira sans font into jakarta plus sans

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import '@fontsource/fira-sans'
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '@/styles/theme'
 import { App } from '@/App'
+
+import '@fontsource/plus-jakarta-sans/400.css'
+import '@fontsource/plus-jakarta-sans/500.css'
+import '@fontsource/plus-jakarta-sans/600.css'
+import '@fontsource/plus-jakarta-sans/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,8 @@
 import { theme as DefaultTheme, extendTheme } from '@chakra-ui/react'
 
 const fonts = {
-  heading: `'Fira Sans', ${DefaultTheme.fonts.heading}`,
-  body: `'Fira Sans', ${DefaultTheme.fonts.body}`
+  heading: `'Plus Jakarta Sans', ${DefaultTheme.fonts.heading}`,
+  body: `'Plus Jakarta Sans', ${DefaultTheme.fonts.body}`
 }
 
 const colors = {
