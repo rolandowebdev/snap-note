1a36d50fbe6ca2c3a3740a67187f83c0bbef813d
rolandowebdev <rolandopranata@gmail.com>
2023-12-09 21:31:35 +0700

    feat: create search and add note button component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,15 @@
-import { Frown } from 'lucide-react'
-import { Container, Box, VStack, Text, Heading } from '@chakra-ui/react'
+import { Frown, Search, Plus } from 'lucide-react'
+import {
+  Container,
+  Box,
+  VStack,
+  Text,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  IconButton
+} from '@chakra-ui/react'
 import { NoteCard } from '@/components/ui'
 import { useNotes } from '@/context'
 
@@ -9,12 +19,43 @@ export const Home = () => {
   return (
     <>
       <Container maxW='container.sm' minH='100vh'>
-        <Box as='header' py='24px'>
+        <Box as='header' py='1rem'>
           <Heading as='h1' size='xl' fontWeight='700' textAlign='center'>
             Snap Note
           </Heading>
+          <Box as='nav' mt={6} display='flex' gap={3} alignItems='center'>
+            <InputGroup>
+              <InputLeftAddon
+                pr={0}
+                pl={4}
+                border={0}
+                h={12}
+                bg='brand.softDark'
+                color='gray.500'
+                children={<Search size={22} />}
+              />
+              <Input
+                type='search'
+                placeholder='Search Note'
+                border={0}
+                h={12}
+                bg='brand.softDark'
+              />
+            </InputGroup>
+            <IconButton
+              h={12}
+              w={14}
+              variant='ghost'
+              aria-label='add note'
+              bg='brand.softDark'
+              color='gray.500'
+              _hover={{ bgColor: 'brand.border' }}
+              _active={{ bgColor: 'transparent' }}
+              icon={<Plus />}
+            />
+          </Box>
         </Box>
-        <Box as='main' py={10}>
+        <Box as='main' pb={10}>
           <VStack as='article' gap={3}>
             {notes.length > 0 ? (
               notes
